e26b5563207680a116c1ad17e66c9c6c0317ee9f
Gintautas Miliauskas <gintautas@miliauskas.lt>
2012-06-28 00:08:12 +0200

    Added Mod-X -> "lock screen" key binding.

--- xmonad.hs
+++ xmonad.hs
@@ -12,4 +12,5 @@ main = xmonad gnomeConfig
   where
     mykeys (XConfig {modMask = modm}) = M.fromList $
          [ ((modm , xK_w), spawn "google-chrome") 
+         , ((modm , xK_x), spawn "gnome-screensaver-command --lock") 
          , ((modm , xK_e), spawn "emacs")]

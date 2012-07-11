4c27910c632d1a9c58fc4f4941cc3c22ae26ea5c
Gintautas Miliauskas <gintas@google.com>
2012-07-11 11:19:54 +0200

    Cosmetic shuffling.

--- xmonad.hs
+++ xmonad.hs
@@ -15,6 +15,7 @@ main = xmonad gnomeConfig
          }
   where
     mykeys (XConfig {modMask = modm}) = M.fromList $
-         [ ((modm , xK_w), spawn "google-chrome")
-         , ((modm , xK_x), spawn "gnome-screensaver-command --lock")
-         , ((modm , xK_e), spawn "emacs")]
+         [ ((modm , xK_x), spawn "gnome-screensaver-command --lock")
+         , ((modm , xK_w), spawn "google-chrome")
+         , ((modm , xK_e), spawn "emacs")
+         ]

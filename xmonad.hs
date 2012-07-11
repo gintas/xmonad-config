0f8bc9bfe9250f81331354b10153d7b2d4df6eaf
Gintautas Miliauskas <gintas@google.com>
2012-07-11 11:18:01 +0200

    Added the setWMName workaround (commented out).

--- xmonad.hs
+++ xmonad.hs
@@ -2,6 +2,7 @@ import XMonad
 import XMonad.Config.Gnome
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.SetWMName
 import qualified Data.Map as M
 
 main = xmonad gnomeConfig
@@ -10,9 +11,10 @@ main = xmonad gnomeConfig
          , keys = \c -> mykeys c `M.union` keys gnomeConfig c
          , manageHook = manageHook gnomeConfig <+> composeOne [ isFullscreen -?> doFullFloat ]
          , handleEventHook = fullscreenEventHook
+         -- , startupHook = setWMName "LG3D"  -- Workaround for some Java GUI apps
          }
   where
     mykeys (XConfig {modMask = modm}) = M.fromList $
-         [ ((modm , xK_w), spawn "google-chrome") 
-         , ((modm , xK_x), spawn "gnome-screensaver-command --lock") 
+         [ ((modm , xK_w), spawn "google-chrome")
+         , ((modm , xK_x), spawn "gnome-screensaver-command --lock")
          , ((modm , xK_e), spawn "emacs")]
